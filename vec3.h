243cda132d0dd8c7f2a91cd99b13d18cd25db6f2
Victor Krassovsky <victorkrassovsky@hotmail.com>
2026-01-23 23:12:50 -0800

    wrote vec3

---
+++ vec3.h
@@ -0,0 +1,114 @@
+#pragma once
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+class vec3{
+ public:
+  double e[];
+ vec3() : e{0,0,0} {}
+ vec3(double e0, double e1, double e2) : e{e0,e1,e2} {}
+  double x(){
+    return e0;
+  }
+  double y(){
+    return e1;
+  }
+  double z(){
+    return e2;
+  }
+
+  double operator - () const{
+    return vec3(-e[0], e[1], e[2]);
+  }
+
+  double operator [] (int i) const{
+    return e[i];
+  }
+
+  double& operator [] (int i) const{
+    return e[i];
+  }
+
+  vec3& operator + (const vec3& v){
+    e[0] += v.x();
+    e[1] += v.y();
+    e[2] += v.z();
+    return *this;
+  }
+
+  vec3& operator * (double t){
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+  }
+
+  vec3& operator / (double t){
+    return *this * (1/t);
+  }
+
+  double length_squared(){
+    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+  }
+  
+  double length(){
+    return std::sqrt(length_squared());
+  }
+};
+
+using point3 = vec3;
+
+
+// inline utility functions for vectors
+inline std::ostream& operator<< (std::ostream& out, vec3& v){
+  return out << v.x() << " " < v.y() << " " << v.z();
+}
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+  return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+  return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+  return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+  return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+  return t * v;
+}
+
+inline vec3 operator/(const vec3& v, double t) {
+  return (1/t) * v;
+}
+
+
+inline double dot(const vec3& u, const vec3& v) {
+  return u.e[0] * v.e[0]
+    + u.e[1] * v.e[1]
+    + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+  return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+	      u.e[2] * v.e[0] - u.e[0] * v.e[2],
+	      u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v) {
+  return v / v.length();
+}
+
+#endif
