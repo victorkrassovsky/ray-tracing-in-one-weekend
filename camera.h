3ca27e99c8dd49655ee6807dd5660826bfcbf076
Victor Krassovsky <victorkrassovsky@hotmail.com>
2026-02-20 10:34:55 -0800

    shadow acne fixed

--- camera.h
+++ camera.h
@@ -8,6 +8,7 @@ class camera {
   double aspect_ratio = 1.0;
   int image_width = 100;
   int samples_per_pixel = 10; // count of random samples for each pixel (for antialiasing)
+  int max_depth = 10; // max number of ray bounces
   
   void render(const hittable& world) {
     initialize();
@@ -20,9 +21,9 @@ class camera {
 	color pixel_color(0,0,0);
 	for (int sample = 0; sample < samples_per_pixel; sample++) {
 	  ray r = get_ray(i, j);
-	  pixel_color += ray_color(r, world);
+	  pixel_color += ray_color(r, max_depth, world);
 	}
-	write_color(std::cout, pixel_color / samples_per_pixel);
+	write_color(std::cout, pixel_color * pixel_samples_scale);
       }
     }
 
@@ -36,11 +37,13 @@ class camera {
   point3 pixel00_loc; // upper left pixel inside the viewport 
   vec3 pixel_delta_u; // horizontal incremental vector between pixels
   vec3 pixel_delta_v; // vertical incremental vector between pixels
-  // double pixel_samples_scale; // color scale factor (antialiasing)
+  double pixel_samples_scale; // color scale factor (antialiasing)
   
   void initialize(){
     image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    pixel_samples_scale = 1.0 / samples_per_pixel;
     
     // Camera
     double focal_length = 1.0; // distance from camera to viewport
@@ -77,11 +80,13 @@ class camera {
   }
   
   // given a ray and hittable, what color should the ray be when it hits
-  color ray_color(const ray& r, const hittable& world) const {
+  color ray_color(const ray& r, int depth, const hittable& world) const {
+    if (depth <= 0)
+      return color(0,0,0);
     hit_record rec;
-    if (world.hit(r, interval(0,infinity), rec)) {
+    if (world.hit(r, interval(0.001,infinity), rec)) {
       vec3 direction = random_on_hemisphere(rec.normal);
-      return 0.5 * (ray_color(ray(rec.p, direction), world));
+      return 0.5 * (ray_color(ray(rec.p, direction), depth-1, world));
     }
   
     vec3 unit_direction = unit_vector(r.direction());
