472eeb5fb57f8a436327bfadc2095b004ed8b023
Victor Krassovsky <victorkrassovsky@hotmail.com>
2026-02-22 20:09:04 -0800

    camera pos and fov fixed

--- main.cpp
+++ main.cpp
@@ -12,20 +12,28 @@ int main(){
 
   hittable_list world;
 
-  double r = std::cos(pi/4);
+  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+  auto material_left   = make_shared<dialectric>(1.50);
+  auto material_bubble = make_shared<dialectric>(1.00 / 1.50);
+  auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-  auto material_left  = make_shared<lambertian>(color(0,0,1));
-  auto material_right = make_shared<lambertian>(color(1,0,0));
-
-  world.add(make_shared<sphere>(point3(-r, 0, -1), r, material_left));
-  world.add(make_shared<sphere>(point3( r, 0, -1), r, material_right));
+  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
+  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
   
   camera cam;
   cam.aspect_ratio = 16.0/9.0;
   cam.image_width = 400;
   cam.samples_per_pixel = 100;
   cam.max_depth = 50;
-  cam.vfov = 90;
+  
+  cam.vfov = 20;
+  cam.camera_pos = point3(-2,2,1);
+  cam.lookat = point3(0,0,-1);
+  cam.up = vec3(0,1,0);
 
   cam.render(world);
 }
