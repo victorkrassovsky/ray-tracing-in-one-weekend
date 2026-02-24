8b66549abc740ea96b66f499aed030c84e83ef47
Victor Krassovsky <victorkrassovsky@hotmail.com>
2026-02-24 15:46:26 -0800

    fixed triangele bug

--- main.cpp
+++ main.cpp
@@ -11,19 +11,21 @@
 int main(){
   hittable_list world;
 
-  //shared_ptr<material> background = make_shared<lambertian>(color(0.4,0.4,0.4));
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, ground_material));
+  
   shared_ptr<material> metal_left = make_shared<metal>(color(0.7, 0.2, 0.2), 0.1);
   shared_ptr<material> metal_right = make_shared<metal>(color(0.2, 0.2, 0.7), 0.2);
   shared_ptr<material> middle = make_shared<lambertian>(color(0.7, 0.1, 0.7));
 
-  //world.add(make_shared<sphere>(point3(0,0,500), 450, background));
-  world.add(make_shared<triangle>(point3(0,0,4),point3(0,1,4),point3(1,0,4), middle));
+  //world.add(make_shared<sphere>(point3(0,1,4), 1, metal_left));
+  world.add(make_shared<triangle>(point3(-1,0,5),point3(0,1,3),point3(1,0,5), middle));
   
   camera cam;
 
   cam.aspect_ratio      = 16.0 / 9.0;
   cam.image_width       = 400;
-  cam.samples_per_pixel = 100;
+  cam.samples_per_pixel = 30;
   cam.max_depth         = 50;
 
   cam.vfov     = 90;
--- triangle.h
+++ triangle.h
@@ -11,25 +11,31 @@ class triangle : public hittable {
   shared_ptr<material> mat;
 
  public:
- triangle(const point3& p1, const point3& p2, const point3& p3, shared_ptr<material> mat) : p1(p1), p2(p2), p3(p3), n(unit_vector(cross(p1 - p2, p1 - p3))), d(dot(n,p1)), mat(mat) {}
+ triangle(const point3& p1, const point3& p2, const point3& p3, shared_ptr<material> mat) : p1(p1), p2(p2), p3(p3),  mat(mat) {
+    n = unit_vector(cross(p2-p1, p3-p1));
+    d = dot(n,p1);
+  }
 
-  bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+  bool hit(const ray& r, interval ray_t, hit_record& rec) const override {    
     vec3 R = r.direction();
     point3 Q = r.origin();
-    if (dot(n, Q) == 0.0){
+    if (std::fabs(dot(n, R)) < 0.01){
       return false;
     }
 
-    double t = (d - dot(n, Q)) / dot(n,R);
+    double t = (d - dot(n, Q)) / dot(n, R);
+    
+    
     if (!ray_t.contains_strict(t))
       return false;
 
     point3 inter = r.at(t);
 
-    if (dot(n, cross(p1 - p2, p1 - inter)) < 0.0 ||
-	dot(n, cross(p2 - p3, p2 - inter)) < 0.0 ||
-	dot(n, cross(p3 - p1, p3 - inter)) < 0.0)
+    if (dot(n, cross(p2 - p1, inter - p1)) < 0.0 ||
+	dot(n, cross(p3 - p2, inter - p2)) < 0.0 ||
+	dot(n, cross(p1 - p3, inter - p3)) < 0.0){
       return false;
+    }
 
     rec.t = t;
     rec.p = inter;
