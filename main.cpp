8901ee158bcbcc9150d2d25a0a11d54a4dec8960
Victor Krassovsky <victorkrassovsky@hotmail.com>
2026-02-24 17:40:16 -0800

    triangle image

--- main.cpp
+++ main.cpp
@@ -11,24 +11,39 @@
 int main(){
   hittable_list world;
 
-  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  auto ground_material = make_shared<lambertian>(color(0.8, 0.8, 0.8));
   world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, ground_material));
-  
-  shared_ptr<material> metal_left = make_shared<metal>(color(0.7, 0.2, 0.2), 0.1);
-  shared_ptr<material> metal_right = make_shared<metal>(color(0.2, 0.2, 0.7), 0.2);
-  shared_ptr<material> middle = make_shared<lambertian>(color(0.7, 0.1, 0.7));
+
+  shared_ptr<material> left = make_shared<lambertian>(color(0.3, 0.8, 0.3));
+  shared_ptr<material> middle = make_shared<lambertian>(color(0.7, 0.7, 0.7));
+  shared_ptr<material> right = make_shared<lambertian>(color(0.1, 0.1, 0.4));
+
+  shared_ptr<material> glass = make_shared<dielectric>(1.2);
+
+  shared_ptr<material> clear_metal = make_shared<metal>(color(0.6,0.6,0.6), 0.1);
 
   //world.add(make_shared<sphere>(point3(0,1,4), 1, metal_left));
-  world.add(make_shared<triangle>(point3(-1,0,5),point3(0,1,3),point3(1,0,5), middle));
+  world.add(make_shared<triangle>(point3(-1,-0.75,5),point3(0,0.25,3),point3(1,-0.75,5), clear_metal));
+  world.add(make_shared<triangle>(point3(-1,-0.75,5),point3(0,0.25,3),point3(-0.75,0.25,2), left));
+  world.add(make_shared<triangle>(point3(0.75,0.25,2),point3(0,0.25,3),point3(1,-0.75,5), right));
+
+  world.add(make_shared<sphere>(point3(-0.5, -0.25, 2.3), 0.25, glass));
+
+  world.add(make_shared<triangle>(point3(0.5, -0.25, 1.5),
+				  point3(0, 0.125, 2.75),
+				  point3(0, -0.5, 2.5),
+				  clear_metal));
+  
+  world.add(make_shared<sphere>(point3(0, -0.3, 3), 0.1, middle));
   
   camera cam;
 
   cam.aspect_ratio      = 16.0 / 9.0;
-  cam.image_width       = 400;
-  cam.samples_per_pixel = 30;
-  cam.max_depth         = 50;
+  cam.image_width       = 1600;
+  cam.samples_per_pixel = 200;
+  cam.max_depth         = 40;
 
-  cam.vfov     = 90;
+  cam.vfov     = 25;
   cam.camera_pos = point3(0,0,0);
   cam.lookat   = point3(0,0,5);
   cam.up      = vec3(0,1,0);
